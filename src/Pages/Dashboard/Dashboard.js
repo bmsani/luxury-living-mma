1ae32e318aba62ddbc2436a9bbdf6f3b9a8cd97c
Majharul tanvir <mdtanvir6070@gmail.com>
2022-07-14 13:59:20 +0600

    Dah route set

--- src/Pages/Dashboard/Dashboard.js
+++ src/Pages/Dashboard/Dashboard.js
@@ -6,14 +6,14 @@ const Dashboard = () => {
         <div className="drawer drawer-mobile">
             <input id="my-drawer-2" type="checkbox" className="drawer-toggle" />
             <div className="drawer-content flex flex-col items-center bg-green-100 h-screen">
-                {/* <!-- Page content here --> */}
+                <h5 className='text-2xl font-bold py-2'>Your dashboard</h5>
                 <Outlet></Outlet>
             </div>
             <div className="drawer-side">
                 <label htmlFor="my-drawer-2" className="drawer-overlay"></label>
                 <ul className="menu p-4 overflow-y-auto w-full bg-base-100 text-base-content">
                     {/* <!-- Sidebar content here --> */}
-                    <li><a href='book'>Book</a></li>
+                    <li><a href='/dashboard'>Book</a></li>
                     <li><a href='review'>Review</a></li>
                     <li><a href='orderList'>Order List</a></li>
                     <li><a href='addService'>Add service</a></li>
