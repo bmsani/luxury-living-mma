bdc6dbb45fa38e58047aad2a0835aeeb588d76f9
ArafatSadi1 <arafatsadi1@gmail.com>
2022-07-14 15:37:51 +0600

    order list done

--- src/Pages/Dashboard/OrderList/OrderList.js
+++ src/Pages/Dashboard/OrderList/OrderList.js
@@ -1,11 +1,40 @@
-import React from 'react';
+import React, { useEffect, useState } from "react";
 
 const OrderList = () => {
-    return (
-        <div>
-            <h1>Hello world</h1>
-        </div>
-    );
+    const [orders, setOrders] = useState([]);
+    useEffect(()=>{
+        fetch("/orders/Orders.json")
+        .then(res => res.json())
+        .then(data => setOrders(data))
+    },[]);
+  return (
+    <div className="w-full px-8">
+      <table className="table w-full">
+        <thead>
+          <tr>
+            <th></th>
+            <th>Name</th>
+            <th>Email</th>
+            <th>Service</th>
+            <th>Pay With</th>
+            <th>Status</th>
+          </tr>
+        </thead>
+        <tbody>
+          {
+            orders.map((order, index) => <tr>
+              <td>{index + 1}</td>
+              <td>{order.name}</td>
+              <td>{order.email}</td>
+              <td>{order.service}</td>
+              <td>{order.payWith}</td>
+              <td>{order.paid ? <button class="btn btn-sm btn-success text-white">Paid</button> : <button class="btn btn-sm btn-error text-white">Unpaid</button> }</td>
+            </tr>)
+          }
+        </tbody>
+      </table>
+    </div>
+  );
 };
 
 export default OrderList;
