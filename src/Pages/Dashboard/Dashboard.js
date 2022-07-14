b5dde21217df6948a499fea8918d3acc7ba69c8e
Majharul tanvir <mdtanvir6070@gmail.com>
2022-07-14 13:43:59 +0600

    dashbord route set

--- src/Pages/Dashboard/Dashboard.js
+++ src/Pages/Dashboard/Dashboard.js
@@ -1,18 +1,23 @@
 import React from 'react';
+import { Outlet } from 'react-router-dom';
 
 const Dashboard = () => {
     return (
         <div className="drawer drawer-mobile">
             <input id="my-drawer-2" type="checkbox" className="drawer-toggle" />
+            <div className="drawer-content flex flex-col items-center bg-green-100 h-screen">
+                {/* <!-- Page content here --> */}
+                <Outlet></Outlet>
+            </div>
             <div className="drawer-side">
                 <label htmlFor="my-drawer-2" className="drawer-overlay"></label>
-                <ul className="menu p-4 overflow-y-auto w-full bg-base-300 text-base-content">
+                <ul className="menu p-4 overflow-y-auto w-full bg-base-100 text-base-content">
                     {/* <!-- Sidebar content here --> */}
-                    <li><a href='/book'>Book</a></li>
-                    <li><a href='/review'>Review</a></li>
-                    <li><a href='/orderList'>Order List</a></li>
-                    <li><a href='/addService'>Add service</a></li>
-                    <li><a href='/makeAdmin'>Make admin</a></li>
+                    <li><a href='book'>Book</a></li>
+                    <li><a href='review'>Review</a></li>
+                    <li><a href='orderList'>Order List</a></li>
+                    <li><a href='addService'>Add service</a></li>
+                    <li><a href='makeAdmin'>Make admin</a></li>
                 </ul>
 
             </div>
