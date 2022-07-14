5e0505525dd86ff7608a08fa4b334909bd663ae1
ArafatSadi1 <arafatsadi1@gmail.com>
2022-07-14 21:22:22 +0600

    make admin done

--- src/Pages/Dashboard/MakeAdmin/MakeAdmin.js
+++ src/Pages/Dashboard/MakeAdmin/MakeAdmin.js
@@ -1,11 +1,18 @@
-import React from 'react';
+import React from "react";
 
 const MakeAdmin = () => {
-    return (
-        <div>
-            
-        </div>
-    );
+  return (
+    <div className="card w-11/12 h-56 bg-base-100 shadow-xl">
+      <div className="flex p-8">
+        <input
+          type="text"
+          placeholder="Email"
+          class="input input-bordered w-full max-w-xs"
+        />
+        <button class="btn ml-2">Submit</button>
+      </div>
+    </div>
+  );
 };
 
 export default MakeAdmin;
